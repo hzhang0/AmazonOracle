b5c8d0aa3326dd3e597d3bb2f22f4f5a0c6f72cb
Haoran Zhang <zhangh87@mcmaster.ca>
2018-01-27 12:32:55 -0500

    Adds basic camera code for app

--- react-native/AmazonOracle/App.js
+++ react-native/AmazonOracle/App.js
@@ -1,23 +1,55 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
-    );
+import React, { Component } from 'react'
+import { ImagePicker } from 'expo'
+import { View,  Button,  ToastAndroid } from 'react-native'
+//https://viblo.asia/p/how-to-upload-image-from-library-or-camera-with-crna-Qbq5QgBzZD8
+//https://docs.expo.io/versions/latest/sdk/imagepicker.html
+export default class HomeScreen extends Component{
+    constructor(props) {
+        super(props)
+    }
+      takePhoto = async () => {
+        let pickerResult = await ImagePicker.launchCameraAsync({
+          exif: false,
+          allowsEditing: true,
+          quality: 0.7,
+          base64: true,
+          aspect: [4, 3]
+        })
+        if (!pickerResult.cancelled) {
+          this.handleUploadPhoto(pickerResult)
+        }
+    }
+    
+    choosePhoto = async () => {
+        let pickerResult = await ImagePicker.launchImageLibraryAsync({
+          exif: false,
+          allowsEditing: false,
+          quality: 0.7,
+          base64: true
+        })
+        if (!pickerResult.cancelled) {
+          this.handleUploadPhoto(pickerResult)
+        }
+     }
+     
+     handleUploadPhoto = pickerResult => {
+        ToastAndroid.show(pickerResult.width.toString(), ToastAndroid.SHORT);
   }
+     
+    render() {
+        return(
+            <View style={{marginTop:20}}>
+                <Button
+                    onPress={this.takePhoto}
+                    title={'Take a photo'}
+                />
+               <Button 
+                    onPress={this.choosePhoto}
+                    title={'Choose a photo'}
+                />
+            </View>)
+        
+    }
+    
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+     
