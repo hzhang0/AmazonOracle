45d07df89ef19109a052f7ba8dfff8a32b35fcb9
Haoran Zhang <zhangh87@mcmaster.ca>
2018-01-28 10:54:39 -0500

    Server now pulls updated prices from Amazon

--- backend/views.py
+++ backend/views.py
@@ -16,30 +16,56 @@ from PIL import Image
 from io import BytesIO
 import base64
 import logging
+from amazon.api import AmazonAPI
 
+keys = json.load(open(os.path.join(settings.STATIC_ROOT, 'endpoint/keys.json'),'r'))
+pd.options.mode.chained_assignment = None
 logger = logging.getLogger('django')
+amazon = AmazonAPI(keys['access_key'], keys['secret_key'], keys['associate_id'], region = 'CA', MaxQPS = 0.9)
 
 def load_array(fname):
     return bcolz.open(fname)[:]
 
 df = pd.read_csv(os.path.join(settings.STATIC_ROOT, 'endpoint/itemSubset.csv')).set_index('asin')
+bns = pd.read_csv(os.path.join(settings.STATIC_ROOT, 'endpoint/BrowseNodes.csv'))
 mapping = {j:i for i,j in enumerate(np.sort(df.search_index.unique()))}
 reverseMapping = {i:j for i,j in enumerate(np.sort(df.search_index.unique()))}
+indexToDep = {i[1]['Search Index']: i[1]['Department'] for i in bns.iterrows()}
 final_model = load_model(os.path.join(settings.STATIC_ROOT, 'endpoint/Weights/final_model'))
 fingerprints = load_array(os.path.join(settings.STATIC_ROOT, 'endpoint/Weights/all_features_inception'))
 similar_images_to_get = 10
 
+def updateData(df): #requests updated item info from amazon
+    ans = amazon.lookup(ItemId = ', '.join(df.index))
+    dfNEW = pd.DataFrame({'price': [float(i.price_and_currency[0]) if i.price_and_currency[0] is not None and not (pd.isnull(i.price_and_currency[0])) else None for i in ans],
+                         'image_url': [i.large_image_url for i in ans],
+                         'title': [i.title for i in ans],
+                         'asin': [i.asin for i in ans]}).set_index('asin')
+    logger.debug(dfNEW.price)
+    logger.debug(df.price)
+    for i in df.index:
+        if i not in dfNEW.index:
+            df.loc[i, 'exists'] = False
+        else:
+            if dfNEW.loc[i, 'title'] is None or dfNEW.loc[i, 'price'] is None or pd.isnull(dfNEW.loc[i,'price']):
+                df.loc[i, 'exists'] = False
+            else:
+                df.loc[i, 'exists'] = True
+                for j in dfNEW.columns:
+                    df.loc[i, j] = dfNEW.loc[i,j]
+    return df
+
 def predict(img):
     X = np.expand_dims((2 * (img_to_array(img) / 255.0 - 0.5)), axis = 0)    
     price, cat, fingerprint = final_model.predict(X)
-    return (price, reverseMapping[np.argmax(cat[0])], findSimilar(fingerprint)) 
+    return (price, indexToDep[reverseMapping[np.argmax(cat[0])]], *findSimilar(fingerprint)) 
 
 def findSimilar(fingerprint):
-    mostSimilar = df.iloc[(np.sum(np.square(fingerprints - fingerprint), axis = 1)).argsort()[:similar_images_to_get]][['title','image_url','price']].reset_index()
-    #logger.debug(mostSimilar.columns)
-#['title','image_url','price'].reset_index()
-    mostSimilar.asin = mostSimilar.asin.apply(lambda x: 'http://asin.info/a/' + x)
-    return mostSimilar.to_dict(orient = 'records')
+    mostSimilar = df.iloc[(np.sum(np.square(fingerprints - fingerprint), axis = 1)).argsort()[:similar_images_to_get]][['title','image_url','price']]
+    mostSimilar = updateData(mostSimilar).reset_index()
+    avg_price = float(mostSimilar.price.mean())
+    mostSimilar.asin = mostSimilar.asin.apply(lambda x: 'https://www.amazon.ca/gp/product/' + x)
+    return mostSimilar.to_dict(orient = 'records'), avg_price
 
 @csrf_exempt
 def index(request):
@@ -55,10 +81,11 @@ def index(request):
         pil_img = Image.open(BytesIO(base64.b64decode(a)))
         img = pil_img.resize((299,299), Image.NEAREST)
         response_data ={}
-        price, cat, similarItems = predict(img)
+        price, cat, similarItems, avg_price = predict(img)
         response_data['predicted_price'] = round(float(price[0][0]),2)
         response_data['predicted_cat'] = cat
         response_data['similar_items'] = similarItems
+        response_data['predicted_price_avg'] = round(avg_price,2) 
         logger.error(response_data)
         for count, i in enumerate(response_data['similar_items']):
             i['key'] = count
